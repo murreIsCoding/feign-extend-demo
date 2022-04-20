e6dd3872fb6297fc09894ac029834937205c3fac
LMY <mliu7@heygears.com>
2022-04-20 16:41:25 +0800

    加注释

--- common/src/main/java/common/UserRest.java
+++ common/src/main/java/common/UserRest.java
@@ -1,9 +1,7 @@
 package common;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author LMY
@@ -11,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  */
 
+/**
+ * 这里不可以使用RequestMapping，所有路径都放到方法上。不然会报错，我认为这带来的不便是可以接受的
+ * 如果需要共存，请查阅https://github.com/spring-cloud/spring-cloud-netflix/issues/466
+ */
 public interface UserRest {
     @GetMapping("/user/findById")
     String findById(@RequestParam Integer id);
